8a556b660755a3927689ea9450918490a36969b9
Jeremy D. Young <jeremydyoung@sbcglobal.net>
2014-06-15 16:40:54 -0500

    Very very basic game client

---
+++ src/main/java/com/jdydev/ew/client/EWClient.java
@@ -0,0 +1,17 @@
+package com.jdydev.ew.client;
+
+import com.jme3.app.SimpleApplication;
+
+public class EWClient extends SimpleApplication {
+
+    public static void main(String[] args) {
+        EWClient app = new EWClient();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // Method must be here, but doesn't have to do anything
+    }
+}
+

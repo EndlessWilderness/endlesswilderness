1ce7582c021d8854e55662a22cbf4d73b4b39975
Jeremy D. Young <jeremydyoung@sbcglobal.net>
2014-06-16 23:34:26 -0500

    Merging the collision tutorial with the terrain tutorial.

--- src/main/java/com/jdydev/ew/client/EWClient.java
+++ src/main/java/com/jdydev/ew/client/EWClient.java
@@ -1,7 +1,23 @@
 package com.jdydev.ew.client;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
 import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
@@ -9,7 +25,20 @@ import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Texture;
 import com.jme3.texture.Texture.WrapMode;
 
-public class EWClient extends SimpleApplication {
+public class EWClient extends SimpleApplication implements ActionListener {
+
+    // private Spatial sceneModel;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean autoWalk = false;
+    private boolean left = false, right = false, up = false, down = false;
+
+    // Temporary vectors used on each frame.
+    // They here to avoid instantiating new vectors on each frame
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
 
     public static void main(String[] args) {
         EWClient app = new EWClient();
@@ -18,8 +47,7 @@ public class EWClient extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        flyCam.setMoveSpeed(50);
-        rootNode.attachChild(this.createTerrain());
+        this.setupBullet(this.createTerrain());
     }
 
     public TerrainQuad createTerrain() {
@@ -85,5 +113,128 @@ public class EWClient extends SimpleApplication {
         terrain.addControl(control);
 
         return terrain;
+    }
+
+    private void setUpLight() {
+        // We add light so we see the scene
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+        rootNode.addLight(dl);
+    }
+
+    /**
+     * We over-write some navigational key mappings here, so we can add physics-controlled walking
+     * and jumping:
+     */
+    private void setUpKeys() {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W), new MouseButtonTrigger(
+                MouseInput.BUTTON_RIGHT));
+        inputManager.addMapping("Walk", new MouseButtonTrigger(MouseInput.BUTTON_MIDDLE));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Walk");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+    }
+
+    /**
+     * These are our custom actions triggered by key presses. We do not walk yet, we just keep track
+     * of the direction the user pressed.
+     */
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Left")) {
+            left = isPressed;
+        } else if (binding.equals("Right")) {
+            right = isPressed;
+        } else if (binding.equals("Up")) {
+            up = isPressed;
+        } else if (binding.equals("Down")) {
+            down = isPressed;
+        } else if (binding.equals("Jump")) {
+            if (isPressed) {
+                player.jump();
+            }
+        } else if (binding.equals("Walk")) {
+            if (!isPressed)
+                autoWalk = !autoWalk;
+        }
+    }
+
+    /**
+     * This is the main event loop--walking happens here. We check in which direction the player is
+     * walking by interpreting the camera direction forward (camDir) and to the side (camLeft). The
+     * setWalkDirection() command is what lets a physics-controlled player walk. We also make sure
+     * here that the camera moves with player.
+     */
+    @Override
+    public void simpleUpdate(float tpf) {
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up || autoWalk) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
+    }
+
+    private void setupBullet(Node scene) {
+        /** Set up Physics */
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        // bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+
+        // We re-use the flyby camera for rotation, while positioning is handled by physics
+        viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+        flyCam.setMoveSpeed(100);
+        flyCam.setDragToRotate(true);
+        // this is a hack to toggle inverted mouse, because it's not directly a function on the
+        // flyCam.
+        flyCam.onAction("FLYCAM_InvertY", false, 0.05f);
+        setUpKeys();
+        setUpLight();
+
+        // We set up collision detection for the scene by creating a
+        // compound collision shape and a static RigidBodyControl with mass zero.
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(scene);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        scene.addControl(landscape);
+
+        // We set up collision detection for the player by creating
+        // a capsule collision shape and a CharacterControl.
+        // The CharacterControl offers extra settings for
+        // size, stepheight, jumping, falling, and gravity.
+        // We also put the player in its starting position.
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(50);
+        player.setGravity(50);
+        player.setPhysicsLocation(new Vector3f(250, -65, 0));
+
+        // We attach the scene and the player to the rootnode and the physics space,
+        // to make them appear in the game world.
+        rootNode.attachChild(scene);
+        bulletAppState.getPhysicsSpace().add(landscape);
+        bulletAppState.getPhysicsSpace().add(player);
     }
 }

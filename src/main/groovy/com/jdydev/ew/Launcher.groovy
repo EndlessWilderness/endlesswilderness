2332fc85797f2ed67edb41d78f886159f513318f
Jeremy D. Young <jeremydyoung@sbcglobal.net>
2014-07-16 22:04:17 -0500

    Took out the cancel button and attempts to disable fields...

--- src/main/groovy/com/jdydev/ew/Launcher.groovy
+++ src/main/groovy/com/jdydev/ew/Launcher.groovy
@@ -7,21 +7,11 @@ import com.jdydev.ew.client.EWClient
 def ewclient = new EWClient()
 start {
     actions {
-        fxaction(id: 'cancelAction', name: 'Cancel', onAction: { stage.close() })
         fxaction(id: 'loginAction', name: 'Login', onAction: {
-            username.disable = true
-            password.disable = true
-            loginButton.disable = true
-            // I want to dump this call to another thread
             if (ewclient.authenticate(username.text, password.text)) {
                 stage.close()
                 ewclient.showSettings = false
                 ewclient.start()
-            } else {
-                println 'Authentication Failed'
-                username.disable = false
-                password.disable = false
-                loginButton.disable = false
             }
         })
     }
@@ -31,10 +21,7 @@ start {
                 text(text: 'Endless Wilderness', font: '36pt sansserif') { fill green }
                 textField(id: 'username', style: '-fx-alignment: center; -fx-font-size: 18')
                 passwordField(id: 'password', style: '-fx-alignment: center; -fx-font-size: 18')
-                hbox(style: '-fx-alignment: center; -fx-spacing: 20') {
-                    button(id: 'loginButton', loginAction)
-                    button(id: 'cancelButton', cancelAction)
-                }
+                button(id: 'loginButton', loginAction)
             }
         }
     }

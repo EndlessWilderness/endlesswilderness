cbae898f8da0b6b27cfdb1593397e1048ae308eb
Jeremy D. Young <jeremydyoung@sbcglobal.net>
2014-07-19 17:54:29 -0500

    Creating a server launch script

---
+++ launchServer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ew="endlesswilderness"
+
+dir="$(ls -1d ../$ew-*)"
+lock="lock.pid"
+if [ ! -e "$dir/$lock" ];then
+  rm -r ../$dir
+  git pull
+  ./gradlew distZip
+  cd ..
+  unzip $ew/build/distributions/$ew*.zip
+
+  dir="$(ls -1d $ew-*)"
+  cd $dir
+  sed -i "s/com.jdydev.ew.Launcher/com.jdydev.ew.server.EWServer/g" bin/$ew
+
+  nohup bin/$ew > server.log 2>&1 &
+  echo $! > $lock
+  echo "Launched EWServer"
+else
+  echo "Not launching EWServer, lock file exists $dir/$lock"
+fi
